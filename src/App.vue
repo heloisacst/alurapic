aa677ad0af5c370ba49424b2c8eba785c890774a
Heloisa Cristina dos Santos <heloisa.santos@zitrus.com.br>
2023-05-04 15:51:56 -0300

    First commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="corpo">
+    <menus :rotas="routes"></menus>
+    <transition name="pagina">
+      <router-view></router-view>
+    </transition>
+  </div>
+</template>
+
+<script>
+import { routes } from './routes';
+import Menu from './components/shared/menu/Menu.vue'
+
+export default {
+
+  components: {
+    'menus' : Menu
+  },
+
+  data () {
+    return {
+      routes
+    }
+  }
+  
+}
+
+</script>
+
+<style>
+
+  .corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+  }
+
+  .pagina-enter, .pagina-leave-active {
+        opacity: 0;
+    }
+    
+  .pagina-enter-active, .pagina-leave-active {
+      transition: opacity .4s;
+  }
+
+</style>
---
+++ src/components/cadastro/Cadastro.vue
@@ -0,0 +1,9 @@
+<template>
+    <h2 class="titulo">Cadastro</h2>
+</template>
+<script></script>
+<style>
+    .titulo {
+        color: blue;
+    }
+</style>
